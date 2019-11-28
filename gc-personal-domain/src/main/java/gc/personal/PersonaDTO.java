0303d1454c3410c15c309f9144a749cdc595e9bc
Pedro <psanchart@hotmail.com>
2019-11-28 14:42:28 +0100

    Add initial project

---
+++ gc-personal-domain/src/main/java/gc/personal/PersonaDTO.java
@@ -0,0 +1,13 @@
+package gc.personal;
+
+public class PersonaDTO {
+
+    private Integer id;
+    private String name;
+
+    public PersonaDTO() {
+        this.id = null;
+        this.name = null;
+    }
+    
+}
---
+++ gc-personal-ejb/src/main/java/gc/personal/PersonaEJB.java
@@ -0,0 +1,9 @@
+package gc.personal;
+
+
+public class PersonaEJB {
+    public PersonaDTO buscarPersonaPorNombre() {
+        return new PersonaDTO();
+    }
+    
+}
